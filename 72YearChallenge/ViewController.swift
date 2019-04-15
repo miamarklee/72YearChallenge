8ae03b6b45fa7d417e98c97b4d6ce008da3ff90d
mia <mia_marklee@hotmail.com>
2019-04-15 20:14:36 +0800

    Initial Commit

---
+++ 72YearChallenge/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  72YearChallenge
+//
+//  Created by apple on 2019/4/15.
+//  Copyright © 2019 apple. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
